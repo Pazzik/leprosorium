0434337825b8a3b7840c2ecb7d7251bc8963dbbf
Pazzik <user@user-NC210-NC110.(none)>
2015-09-06 12:04:12 +0300

    save content to database

--- app.rb
+++ app.rb
@@ -10,17 +10,17 @@ end
 
 before do
 	init_db
-
 end
 
-configure do 
+configure do
+	init_db 
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		created_date DATE,
 		content TEXT
 	)'
-end
+end	
 
 
 get '/' do
@@ -37,6 +37,8 @@ post '/new' do
   		@error = "Type post text"
   		return erb :new
   	end
+
+  	@db.execute 'insert into Posts (content,created_date) values (?,datetime())', [content]
   	erb "You typed #{content}"
 end
 
